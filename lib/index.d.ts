655caa8860287c6f666274081e13f2b01f890360
qrj <myused@sina.com>
2022-09-05 12:23:49 +0800

    !

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,13 +1,4 @@
-import asyncSleep from './asyncSleep';
-import createRequest from './service';
-import Store from './Store';
-declare const _default: {
-    asyncSleep: typeof asyncSleep;
-    createRequest: typeof createRequest;
-    Store: typeof Store;
-    isNotEmptyArray<T = any>(value: any): value is T[];
-    isSameNull(value: any): value is null;
-    isNumberWithString(obj: any): obj is number;
-    isEmptyPropsValue(value: any): value is "";
-};
-export default _default;
+export { default as asyncSleep } from './asyncSleep';
+export * as lang from './lang';
+export { default as createRequest } from './service';
+export { default as Store } from './Store';
